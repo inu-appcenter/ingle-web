11308c4076b7e6fce7e37540dec18e365d1701d9
ssuasu <kamijsa2004@naver.com>
2025-08-06 17:30:06 +0900

    ✨ feat : 포탈 ui 구현

--- src/auth/components/Intro.tsx
+++ src/auth/components/Intro.tsx
@@ -1,9 +1,26 @@
+import { useAuthStore } from '../stores/authStore';
+
 export default function Intro() {
+  const currnetStep = useAuthStore(state => state.currentStep);
+  const setStep = useAuthStore(state => state.setStep);
+
   return (
     <div>
-      <h1>Ingle 앱 소개</h1>
-      <p>Ingle 앱은 학생들을 위한 포털 로그인 및 정보 등록을 지원합니다.</p>
+      <h1>ingle</h1>
+      <h1>Your Global Guide Book for INU Students</h1>
+      <p>
+        Everything you need to know about life at INU. All-in-one guide to
+        campus, classes, and daily life.
+      </p>
       {/* 여기에 추가적인 앱 소개 내용을 작성할 수 있습니다 */}
+      <button
+        className="m-8 w-[80%] h-12 border border-color-gray rounded-2xl"
+        onClick={() => {
+          setStep('portal');
+        }}
+      >
+        Get Start
+      </button>
     </div>
   );
 }
