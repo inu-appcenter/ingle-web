f2cded7597b9313b845fe7ae5498c73002b7f714
LimJaeHyun <guswogus81@gmail.com>
2025-08-11 23:01:23 +0900

    ✨ feat : svgr 라이브러리 추가 및 적용

--- src/stamp/components/RewardHeader.tsx
+++ src/stamp/components/RewardHeader.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from 'react';
+import BackIcon from '@/shared/assets/icons/back-icon.svg?react';
 
 const RewardHeader = ({ children }: { children: ReactNode }) => {
   return (
     <header className="w-full h-20 flex items-center gap-2">
-      <img src="" alt="뒤로가기" className="w-8 h-8" />
+      <BackIcon className="cursor-pointer" />
       <p className="text-2xl">{children}</p>
     </header>
   );
