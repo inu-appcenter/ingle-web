108e30c7cd480a41c4a7bd1da881ee9e9934959c
ssuasu <kamijsa2004@naver.com>
2025-08-17 16:47:34 +0900

    ✨ feat : 상세페이지 맨 아래 ,가이드 컴포넌트

--- src/auth/stores/authStore.ts
+++ src/auth/stores/authStore.ts
@@ -3,32 +3,37 @@ import { create } from 'zustand';
 const ACCESS_TOKEN_KEY = 'access_token';
 const REFRESH_TOKEN_KEY = 'refresh_token';
 const REMEMBER_KEY = 'remember';
-// const [Cookies, setCookie] = useCookies(['refresh']);
+
+type ServerResponse = {
+  memberId: number;
+  studentId: string;
+  department: string;
+  studentType: string;
+  nickname: string;
+  accessToken: string;
+  accessTokenExpiresDate: Date;
+};
 
 interface AuthState {
   currentStep: 'intro' | 'portal' | 'studentInfo' | 'finish';
   portalId: string;
 
-  // 정보 입력 단계
+  // 학생 정보
   studentId: string;
   studentType: string;
   department: string;
   nickname: string;
 
-  setStep: (step: AuthState['currentStep']) => void;
-  setStudentInfo: (
-    info: Partial<
-      Omit<AuthState, 'setStep' | 'setPortalStudentId' | 'setStudentInfo'>
-    >,
-  ) => void;
-
   accessToken: string | null;
-  refreshToken: string | null;
-  remember: boolean; //true면 로컬스토리지에 저장, false면 세션스토리지에 저장
-
+  expiryTime: Date | null;
+  remember: boolean; // ? : 아직 잘 모르겠다.
   isAuthenticated: boolean; // 로그인 상태 여부
 
-  setTokens: (access: string, refresh: string, remember?: boolean) => void;
+  setStep: (step: AuthState['currentStep']) => void;
+  setStudentInfo: (
+    info: Partial<Omit<AuthState, 'setStep' | 'setPortalStudentId' | 'setStudentInfo'>>,
+  ) => void;
+  setTokens: (res: ServerResponse) => void;
   clearTokens: () => void;
 }
 
@@ -41,30 +46,26 @@ export const useAuthStore = create<AuthState>((set, get) => ({
   nickname: '',
 
   accessToken: null,
-  refreshToken: null,
+  expiryTime: null,
   remember: false,
   isAuthenticated: false,
 
   setStep: step => set({ currentStep: step }),
   setStudentInfo: info => set(state => ({ ...state, ...info })),
 
-  setTokens: (access: string, refresh: string, remember = false) => {
-    // 저장할 장소 결정
+  // access token, expireDate 저장
+  setTokens: (res: ServerResponse) => {
     try {
-      //remember 여부 저장
-      if (remember) {
-        localStorage.setItem(REFRESH_TOKEN_KEY, refresh);
-        localStorage.setItem(REMEMBER_KEY, '1');
-      } else {
-        sessionStorage.setItem(REFRESH_TOKEN_KEY, refresh);
-        localStorage.removeItem(REMEMBER_KEY);
-      }
-
-      //accessToken은 메모리에만 저장
+      // 메모리에 저장
       set({
-        accessToken: access,
-        remember,
         isAuthenticated: true,
+        accessToken: res.accessToken,
+        expiryTime: res.accessTokenExpiresDate,
+
+        studentId: res.studentId,
+        department: res.department,
+        studentType: res.studentType,
+        nickname: res.nickname,
       });
     } catch (e) {
       console.warn('storage write failed', e);
@@ -74,8 +75,6 @@ export const useAuthStore = create<AuthState>((set, get) => ({
   clearTokens: () => {
     try {
       localStorage.removeItem(REMEMBER_KEY);
-      localStorage.removeItem(REFRESH_TOKEN_KEY);
-      sessionStorage.removeItem(REFRESH_TOKEN_KEY);
     } catch (e) {
       console.warn('storage clear failed', e);
     }
--- src/tutorials/components/InformationButton.tsx
+++ src/tutorials/components/InformationButton.tsx
@@ -1,4 +1,4 @@
-import CylceIcon from '@/shared/assets/icons/cycle-icon.svg';
+import CylceIcon from '@/shared/assets/icons/cycle-icon.svg?react';
 import { useNavigate } from 'react-router';
 
 interface Props {
@@ -17,7 +17,7 @@ const InformationButton = ({ name, description, route }: Props) => {
       className="group flex flex-col text-left hover:bg-[#7A00E6] bg-[#EDEDED] rounded-[20px] h-48 pl-2.5 pt-3 hover:text-[#ffffff] transition-transform duration-300 ease-in-out transform hover:-translate-y-1"
     >
       <div className="flex w-9 h-9 rounded-full group-hover:bg-[#7949FF] bg-[#DADADA] justify-center items-center mb-10">
-        <img src={CylceIcon} alt="아이콘" />
+        <CylceIcon />
       </div>
       <div className="pr-2.5">
         <p className="text-xl font-bold mb-1">{name}</p>
